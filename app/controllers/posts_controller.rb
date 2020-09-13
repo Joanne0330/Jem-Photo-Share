43cdc1b3688865190c4fe3089ed369b0bf4abc31
Joanne Chen <joanne_r_chen@yahoo.com>
2020-09-13 12:52:25 +0100

    able to add and create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,24 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      flash.notice = "Post successfully saved!"
+    else
+      flash.alert = "Post rejected!"
+    end
+
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title)
+  end
 end
