0d3c2c52d2d83e10791aada73db683bd30fd1ff0
Joanne Chen <joanne_r_chen@yahoo.com>
2020-09-30 10:49:13 +0100

    only current user can delete the posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,13 @@ class Post
   include Mongoid::Document
   include Mongoid::Timestamps
   include Mongoid::Paperclip
- 
+  # before_create :owner
+  belongs_to :user
+  
+  # def owner
+  #   self.user_id = current_user.id
+  # end
+
   has_mongoid_attached_file :image,
     :styles => {
         :medium   => ['250x250',    :jpg]
